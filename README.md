07032233f2bd9040f49d392ee48d63ae2382fec6
James Heda Weng <james@Jamess-MacBook-Pro.local>
2018-06-11 00:06:26 +0200

    Add SCSS

---
+++ README.md
@@ -0,0 +1,10 @@
+#    GIF Animation
+##   Test project 2 for StyleSage
+This project includes a grid of GIF images.
+
+###  To start the local server
+npm run serve
+Then visit localhost:8080
+
+###  To build a version for production
+npm run build
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Nice Pictures!</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
+  name: 'GIF Animations',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import './src/assets/scss/pages/main';
 </style>
